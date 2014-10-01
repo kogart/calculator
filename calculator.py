f6c3f5178caa587c74a52cbfae6f8d9b365af3dc
ChristelleWehbe <libanon_cristelle@hotmail.com>
2014-10-01 12:07:37 +0200

    upphöjt

--- calculator.py
+++ calculator.py
@@ -1,12 +1,7 @@
-def divition (a , b):
-    c = a / b
-    return c
-def Plus(a, b):
-    c = a + b
-    return c
-def Minus(a, b):
-    c = a - b
-    return c
 def multiplikation(a, b):
     c = a * b
     return c
+
+def pow(a,x):
+    b = a**x
+    return b
